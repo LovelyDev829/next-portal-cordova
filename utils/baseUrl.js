b21704c44c98b8fe1c3e9f3855e520b7a02f6acb
LovelyDev829 <lovelydeveloper829@gmail.com>
2023-07-11 01:45:23 +0900

    edemy

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://next-portal-cordova-ey8c.vercel.app'
+? 'https://edemy-react.envytheme.com'
 : 'http://localhost:3000';
 
 export default baseUrl;
